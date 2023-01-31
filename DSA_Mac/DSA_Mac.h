0875cadc2c3dfe315fef22c7a766ac3224ed115a
Medhad <medhad@gmail.com>
2023-01-31 12:43:00 -0500

    Initial Commit

---
+++ DSA_Mac/DSA_Mac.h
@@ -0,0 +1,18 @@
+//
+//  DSA_Mac.h
+//  DSA_Mac
+//
+//  Created by Medhad Ashraf Islam on 31/1/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DSA_Mac.
+FOUNDATION_EXPORT double DSA_MacVersionNumber;
+
+//! Project version string for DSA_Mac.
+FOUNDATION_EXPORT const unsigned char DSA_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSA_Mac/PublicHeader.h>
+
+
