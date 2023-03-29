97ece83c848637332679d7b7b39fac93d310c14d
Medhad <medhad@gmail.com>
2023-03-28 22:16:19 -0400

    Fibonacci function using recursion and using loop

---
+++ DSA_Mac/Algorithms/leetcodecrcourse/RecursionProblems.swift
@@ -0,0 +1,33 @@
+//
+//  RecursionProblems.swift
+//  DSA_Mac
+//
+//  Created by Medhad Ashraf Islam on 28/3/23.
+//
+
+import Foundation
+
+//MARK: Using Recursion
+func fibonacci(_ n: Int) -> Int {
+    if n == 0 || n == 1 {
+        return n
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+
+//MARK: Using Loop
+func fibonacciUsingLoop(_ n: Int) -> Int {
+    if n == 0 || n == 1 {
+        return n
+    }
+
+    var fib = [0, 1]
+
+    for i in 2...n {
+        fib.append(fib[i-1] + fib[i-2])
+    }
+
+    return fib[n]
+}
