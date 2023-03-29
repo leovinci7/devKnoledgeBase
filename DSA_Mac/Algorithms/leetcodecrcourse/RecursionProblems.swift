67393572cd2d39d58749efeeb470f867e57cd432
Medhad <medhad@gmail.com>
2023-03-28 22:20:31 -0400

    Added class

--- DSA_Mac/Algorithms/leetcodecrcourse/RecursionProblems.swift
+++ DSA_Mac/Algorithms/leetcodecrcourse/RecursionProblems.swift
@@ -7,27 +7,35 @@
 
 import Foundation
 
-//MARK: Using Recursion
-func fibonacci(_ n: Int) -> Int {
-    if n == 0 || n == 1 {
-        return n
+
+public class RecursionProblem{
+    
+    public init(){
+        }
+    
+    //MARK: Using Recursion
+    func fibonacci(_ n: Int) -> Int {
+        if n == 0 || n == 1 {
+            return n
+        }
+        
+        return fibonacci(n - 1) + fibonacci(n - 2)
     }
-
-    return fibonacci(n - 1) + fibonacci(n - 2)
-}
-
-
-//MARK: Using Loop
-func fibonacciUsingLoop(_ n: Int) -> Int {
-    if n == 0 || n == 1 {
-        return n
+    
+    
+    //MARK: Using Loop
+    func fibonacciUsingLoop(_ n: Int) -> Int {
+        if n == 0 || n == 1 {
+            return n
+        }
+        
+        var fib = [0, 1]
+        
+        for i in 2...n {
+            fib.append(fib[i-1] + fib[i-2])
+        }
+        
+        return fib[n]
     }
-
-    var fib = [0, 1]
-
-    for i in 2...n {
-        fib.append(fib[i-1] + fib[i-2])
-    }
-
-    return fib[n]
+    
 }
