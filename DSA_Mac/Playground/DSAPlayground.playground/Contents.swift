edf56e0ece0a1a435407344fcd888f3b188d19ff
Medhad <medhad@gmail.com>
2023-04-11 15:44:15 -0400

    Added Play ground for quick check

--- DSA_Mac/Playground/DSAPlayground.playground/Contents.swift
+++ DSA_Mac/Playground/DSAPlayground.playground/Contents.swift
@@ -9,11 +9,13 @@ let dict = ["name": "John", "age": 30] as [String : Any]
 // Iterating over an array
 for num in arr {
     // Do something with num
+    print(num)
 }
 
 // Iterating over a dictionary
 for (key, value) in dict {
     // Do something with key and value
+    print("\(key) \(value)")
 }
 
 // Using map to transform an array
@@ -33,4 +35,39 @@ let sortedArr = arr.sorted()
 
 // Reversing an array
 let reversedArr = arr.reversed()
+
+// Array to string
+let testString = "Hello World"
+let stringToArray = Array(testString)
+let arrayToString = String(stringToArray)
+
+
+//For loop:
+let j = 5
+//using while
+var i = 0
+while i < j {
+    i += 1
+}
+
+// using for loop
+for i in 0..<j {
+    // Do something with i
+    print(i)
+}
+//using stride
+// iterates till less than j
+for i in stride(from: 0, to: j, by: 1) {
+    // Do something with i
+    print(i)
+}
+//iterates till equal to j
+for i in stride(from: 0, through: j, by: 1) {
+    // Do something with i
+    print(i)
+}
+
+
+
+
 
